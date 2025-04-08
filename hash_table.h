e7ee8b43520a196a2b916864b848a805e3d3ef21
Landon Craft <133186492+LandoTheDeveloper@users.noreply.github.com>
2025-04-08 15:28:36 -0400

    hash_struct provided

--- hash_table.h
+++ hash_table.h
@@ -3,12 +3,17 @@
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
-#include <pthread.h>
+#include <stdio.h>
+#include <stdint.h>
 
 #define TABLE_SIZE 1000
 
-typedef struct Node {
-    int key;
-    struct Node* next;
-} Node;
+typedef struct hash_struct
+{
+    uint32_t hash;
+    char name[50];
+    uint32_t salary;
+    struct hash_struct *next;
+} hashRecord;
 
+#endif
