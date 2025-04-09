cb48144c9aa8472d2bb73f1d1dcfee3996dd62f5
Bobby <bobbymorris32304@gmail.com>
2025-04-09 11:10:55 -0400

    Added read/write locks file
    Added insert but have not tested yet (sorry for reformatting, i need to
    fit my formatter)

---
+++ read_write_lock.c
@@ -0,0 +1,37 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct _rwlock_t {
+  sem_t writelock;
+  sem_t lock;
+  int readers;
+} rwlock_t;
+
+void rwlock_init(rwlock_t *lock) {
+  lock->readers = 0;
+  sem_init(&lock->lock, 0, 1);
+  sem_init(&lock->writelock, 0, 1);
+}
+
+void rwlock_acquire_readlock(rwlock_t *lock) {
+  sem_wait(&lock->lock);
+  lock->readers++;
+  if (lock->readers == 1)
+    sem_wait(&lock->writelock);
+  sem_post(&lock->lock);
+}
+
+void rwlock_release_readlock(rwlock_t *lock) {
+  sem_wait(&lock->lock);
+  lock->readers--;
+  if (lock->readers == 0)
+    sem_post(&lock->writelock);
+  sem_post(&lock->lock);
+}
+
+void rwlock_acquire_writelock(rwlock_t *lock) { sem_wait(&lock->writelock); }
+
+void rwlock_release_writelock(rwlock_t *lock) { sem_post(&lock->writelock); }
